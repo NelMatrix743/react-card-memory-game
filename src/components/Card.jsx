330ce795557abfc33d4a5cebea3fc99712760108
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-03-01 20:47:25 +0000

    remove the key prop from the card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ const Card = (props) => {
     const {cardContent, handleCallBack } = props;
 
     return (
-        <div className={`card ${cardContent.isFlipped ? "flipped" : ""}`} onClick={() => console.log(`${cardContent.id}`)}>
+        <div className={`card ${cardContent.isFlipped ? "flipped" : ""}`} onClick={() => handleCallBack(cardContent)}>
             <div className="card-front">?</div>
             <div className="card-back">{ cardContent.value  }</div>
         </div>
